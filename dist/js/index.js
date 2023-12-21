a5fd7735653f8ed3c602d2e665d7a1be82944bd1
ELSOLRA <bardakasmiljo@gmail.com>
2023-12-21 15:23:25 +0100

    continuing with functions in class

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,1 +1,90 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+class BeerApp {
+    constructor() {
+        this.searUrlApi = 'https://api.punkapi.com/v2/beers';
+        this.input = document.getElementById('site-search');
+        this.searchedContent = document.getElementById('searched-content');
+        this.currentPage = 1;
+        this.beersPerPage = 10;
+        this.displayedBeers = [];
+        this.totalBeers = 0;
+        this.setupEventListeners();
+    }
+    setupEventListeners() {
+        var _a, _b;
+        (_a = document.getElementById('search-btn')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => this.searchBeer());
+        (_b = document.getElementById('random-beer-btn')) === null || _b === void 0 ? void 0 : _b.addEventListener('click', () => this.getRandomBeer());
+    }
+    getRandomBeer() {
+        return __awaiter(this, void 0, void 0, function* () {
+            this.searchedContent.textContent = '';
+            try {
+                const response = yield fetch(`${this.searUrlApi}/random`);
+                if (!response.ok) {
+                    throw new Error(`HTTP Error!: ${response.status}`);
+                }
+                const beer = yield response.json();
+                this.displayBeer(beer);
+            }
+            catch (error) {
+                console.log("Error fetching message :", error);
+            }
+        });
+    }
+    searchBeer() {
+        return __awaiter(this, void 0, void 0, function* () {
+            this.searchedContent.textContent = '';
+            const searchWord = this.input.value.trim();
+            let pageForTotal = 1;
+            let moreResults = true;
+            let totalResultBeers = [];
+            if (searchWord === '') {
+                return;
+            }
+            try {
+                const response = yield fetch(`${this.searUrlApi}?beer_name=${searchWord}&page=${this.currentPage}&per_page=${this.beersPerPage}`);
+                if (!response.ok) {
+                    throw new Error(`HTTP Error!: ${response.status}`);
+                }
+                const result = yield response.json();
+                console.log(result);
+                this.displayedBeers = result;
+                while (moreResults) {
+                    const totalSearchedBeersResponse = yield fetch(`${this.searUrlApi}?beer_name=${searchWord}&page=${pageForTotal}&per_page=80`);
+                    const data = yield totalSearchedBeersResponse.json();
+                    if (data.length > 0) {
+                        totalResultBeers = totalResultBeers.concat(data);
+                        pageForTotal++;
+                    }
+                    else {
+                        moreResults = false;
+                    }
+                }
+                this.totalBeers = totalResultBeers.length;
+                console.log(this.totalBeers);
+                if (this.displayedBeers.length === 0) {
+                    this.displayNoResults();
+                }
+                else {
+                    this.displayBeerList();
+                    this.updatePageDisplay();
+                }
+            }
+            catch (error) {
+                console.log("Error fetching message :", error);
+            }
+        });
+    }
+    displayNoResults() {
+        this.searchedContent.innerHTML = "<p>No results found for the given search term.</p>";
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,88 @@ interface Beer {
     brewers_tips: string;
 }
 
+class BeerApp {
+
+    private searUrlApi = 'https://api.punkapi.com/v2/beers';
+    private input: HTMLInputElement = document.getElementById('site-search') as HTMLInputElement;
+    private searchedContent: HTMLElement = document.getElementById('searched-content') as HTMLElement;
+    private currentPage: number = 1;
+    private beersPerPage: number = 10;
+    private displayedBeers: Beer[] = [];
+    private totalBeers: number = 0;
+
+    constructor() {
+        this.setupEventListeners();
+    }
+
+    private setupEventListeners() {
+        document.getElementById('search-btn')?.addEventListener('click', () => this.searchBeer());
+        document.getElementById('random-beer-btn')?.addEventListener('click', () => this.getRandomBeer());
+    }
+
+    private async getRandomBeer() {
+        this.searchedContent.textContent = '';
+        try {
+          const response = await fetch(`${this.searUrlApi}/random`);
+          if (!response.ok) {
+            throw new Error(`HTTP Error!: ${response.status}`);
+          }
+          const beer = await response.json() as Beer;
+          this.displayBeer(beer);
+        } catch (error) {
+            console.log("Error fetching message :", error);
+          }
+    }
+
+    private async searchBeer() {
+        this.searchedContent.textContent = '';
+        const searchWord = this.input.value.trim();
+        let pageForTotal = 1; // Start from the next page
+        let moreResults = true;
+        let totalResultBeers: Beer[] = [];
+        if (searchWord === '') {
+          return;
+        }
+        try {
+            const response = await fetch(`${this.searUrlApi}?beer_name=${searchWord}&page=${this.currentPage}&per_page=${this.beersPerPage}`);
+            if (!response.ok) {
+              throw new Error(`HTTP Error!: ${response.status}`);
+            }
+            const result = await response.json() as Beer[];
+            console.log(result);
+            this.displayedBeers = result; 
+            
+            while (moreResults) {
+                const totalSearchedBeersResponse = await fetch(`${this.searUrlApi}?beer_name=${searchWord}&page=${pageForTotal}&per_page=80`);
+                const data = await totalSearchedBeersResponse.json();      
+                
+                if (data.length > 0) {
+                    totalResultBeers = totalResultBeers.concat(data);   
+                    pageForTotal++;
+                } else {
+                    moreResults = false;
+                  }
+            }
+            
+            this.totalBeers = totalResultBeers.length;
+            console.log(this.totalBeers);
+
+            if (this.displayedBeers.length === 0) {
+                this.displayNoResults();
+              } else {
+                this.displayBeerList();
+                this.updatePageDisplay();
+              }
+            } catch (error) {
+              console.log("Error fetching message :", error);
+            }
+        }
+        
+        private displayNoResults() {
+            this.searchedContent.innerHTML = "<p>No results found for the given search term.</p>";
+        }
+        
+            
+
+                
+}
