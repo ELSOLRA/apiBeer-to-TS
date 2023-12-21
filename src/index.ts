43224997f2323027cfe049b117378ce407f76102
ELSOLRA <bardakasmiljo@gmail.com>
2023-12-21 10:56:25 +0100

    beer interface

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+interface Beer {
+    name: string;
+    description: string;
+    image_url: string | null;
+    volume: {
+        value: number;
+        unit: string;
+    };
+    abv: number;
+    ingredients: {
+        hops: { name: string }[];
+        malt: { name: string }[];
+    };
+    food_pairing: string [];
+    brewers_tips: string;
+}
+
